686411054d7d13ab76e98bb7de4ec7529af87c17
Krtpai <kurtmararac.stema@gmail.com>
2024-02-28 14:02:45 +0800

    SBD Day-02

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,5 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+num3 = input("Enter the third number: ")
+sum = int(num1) + int(num2) + int(num3)
+print("Sum:", sum)
