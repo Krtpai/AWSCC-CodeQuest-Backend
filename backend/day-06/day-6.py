6b32c2b00948e7f9bd100ac2f61351a20cffa9e3
Krtpai <kurtmararac.stema@gmail.com>
2024-02-29 18:47:15 +0800

    SBD Day-06

---
+++ backend/day-06/day-6.py
@@ -0,0 +1,17 @@
+def fizz_buzz(limit):
+    for i in range(1, limit + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz!')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+def main():
+    limit = int(input("Limit: "))
+    fizz_buzz(limit)
+
+if __name__ == "__main__":
+    main()
