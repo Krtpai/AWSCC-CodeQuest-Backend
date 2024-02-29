263d53e4056066d3e4cfdc67a92c9ca9cf53d8ee
Krtpai <kurtmararac.stema@gmail.com>
2024-02-29 17:32:09 +0800

    SBD Day-04

--- backend/day-04/day-4.py
+++ backend/day-04/day-4.py
@@ -2,13 +2,9 @@ print("Welcome to Rock-Paper-Scissor Game!")
 p1 = input("Player1: ")
 p2 = input("Player2: ")
 
-if (p1 and p2):
+if p1 == p2:
     print("It's a tie!")
-elif (p1 == "Rock") and (p2 == "Scissor"):
-    print("Player1 Wins!")
-elif (p1 == "Scissor") and (p2 == "Paper"):
-    print("Player1 Wins!")
-elif (p1 == "Paper") and (p2 == "Rock"):
+elif (p1 == "Rock" and p2 == "Scissor") or (p1 == "Scissor" and p2 == "Paper") or (p1 == "Paper" and p2 == "Rock"):
     print("Player1 Wins!")
 else: 
     print("Player2 Wins!")
