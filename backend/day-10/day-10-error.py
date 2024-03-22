a85b0ae0a701f2d5192e31ab82de4cdfd9f7a40c
Krtpai <kurtmararac.stema@gmail.com>
2024-03-22 11:06:03 +0800

    SBD Day-10

--- backend/day-10/day-10-error.py
+++ backend/day-10/day-10-error.py
@@ -5,7 +5,10 @@ try:
     a = int(input("Enter a number: "))
     b = int(input("Enter a second number: "))
     quotient = divide(a, b)
+    print(f"The quotient is {quotient}")
 except Exception as e:
     print(f"Invalid input: {e}")
-else:
-    print(f"The quotient is {quotient}")
+    
+#Removing 'else' makes the program much logical. Since leaving that will still
+#be executed when no exception occurs, but the variable for 'quotient' might not be 
+#defined if an exception occurs within 'try'. 
