031786d3b3861f4772da9f20749553eddbefee2d
Krtpai <kurtmararac.stema@gmail.com>
2024-03-22 10:48:53 +0800

    SBD-DAY 07 & DAY 08

--- backend/day-08/day-8-avg.py
+++ backend/day-08/day-8-avg.py
@@ -8,3 +8,14 @@ for num in my_list:
 
 avg = sum / len(my_list)
 print(avg)
+
+# DEBUGGER NOTES:
+#(1). Breakpoints(red circle), used along the side/gutter of the
+#(2). number lines of code in order to check what is happening within
+#(3). that placed breakpoint. Can place as many as you want if necessary.
+#3 Tools in a Debugger: 
+#(4). STEP OVER - moves into the next line of code.
+#(5). STEP INTO - steps into methods,functions, or anything that is happening
+#                 on the current line.
+#(6). STEP OUT - takes us out the current scope, we go right back at the last statement 
+#                or whatever function we're into. 
